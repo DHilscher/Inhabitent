0b0c284726ad9d99030cd31c9155005c7c7419db
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-14 13:26:51 -0700

    custom post-types added

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,7 +10,7 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info" style="font-family: font-awesome">
+				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
