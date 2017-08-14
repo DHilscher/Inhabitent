432632c574a55e000547e0a8ac02c1a023cf877b
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-14 16:01:05 -0700

    about hero banner

--- themes/inhabitent/inc/extras.php
+++ themes/inhabitent/inc/extras.php
@@ -2,7 +2,7 @@
 /**
  * Custom functions that act independently of the theme templates.
  *
- * @package RED_Starter_Theme
+ * @package inhabitent_Theme
  */
 
 /**
@@ -43,3 +43,25 @@ function inhabitent_login_title() {
     return 'Inhabitent';
 }
 add_filter( 'login_headertitle', 'inhabitent_login_title' );
+
+/**
+ * Make hero image customizable through CFS field or featured image.
+ */
+function inhabitent_dynamic_css() {
+    if ( ! is_page_template( 'page-templates/about.php' ) ) {
+        return;
+    }
+    
+    $image = CFS()->get( 'about_header_image' );
+    if ( ! $image ) {
+        return;
+    }
+    $hero_css = ".page-template-about .entry-header {
+        background:
+            linear-gradient( to bottom, rgba(0,0,0,0.4) 0%, rgba(0,0,0,0.4) 100% ),
+            url({$image}) no-repeat center bottom;
+        background-size: cover, cover;
+    }";
+    wp_add_inline_style( 'tent-style', $hero_css );
+}
+add_action( 'wp_enqueue_scripts', 'inhabitent_dynamic_css' );
