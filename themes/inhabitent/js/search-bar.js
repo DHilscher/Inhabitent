4466c399f4d84b09d312d97d91f187385fdba555
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-20 15:19:33 -0700

    added focus and blur to js search bar

--- themes/inhabitent/js/search-bar.js
+++ themes/inhabitent/js/search-bar.js
@@ -2,7 +2,10 @@ jQuery(document).ready(function($) {
     $('.search-toggle').on('click', function(event) {
         event.preventDefault();  
        $('.search-field').toggle('slow');
-          
+       $(".search-field").focus();   
+    });
+    $('.search-field').blur(function() {
+    $('.search-field').hide();
     });
 });
 
