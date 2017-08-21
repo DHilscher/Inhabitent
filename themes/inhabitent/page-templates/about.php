2f1143c09cb7dbc26b09d21f222ffbe16d3efaf3
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-21 02:34:18 -0700

    styled aboutl page

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -9,18 +9,18 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <header class="entry-header custom-hero">
-            <div class="container">
-            <?php the_title( '<h1 class="entry-title">', '</h1'); ?>
-            <div>
+                <div class="container">
+                    <?php the_title( '<h1 class="entry-title">', '</h1'); ?>
+                <div>
             </header>
-            <div class="container">
-            <div class="entry-container">
-            <h2>Our Story</h2>
-            <?php echo CFS()->get( 'about_our_story' ); ?>
+            <div class="about-container">
+                <div class="entry-container">
+                    <h2>Our Story</h2>
+                    <?php echo CFS()->get( 'about_our_story' ); ?>
 
-            <h2>Our Team</h2>
-            <?php echo CFS()->get( 'about_our_team' ); ?>
-            </div>
+                    <h2>Our Team</h2>
+                    <?php echo CFS()->get( 'about_our_team' ); ?>
+                </div>
             </div>
         </article>
     </main><!-- #main -->
